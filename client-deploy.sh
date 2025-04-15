e887bf4de3ab797cca1485e1f4b6e00027b02ad8
bertoale <albert@Master.>
2025-04-15 13:24:20 +0800

    FISRT

---
+++ client-deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f client-env.yaml
+kubectl apply -f client-deployment.yaml
+kubectl apply -f client-service.yaml
+# kubectl apply -f client-hpa.yaml
+kubectl apply -f client-ingress.yaml
---
+++ server-deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f server-env.yaml
+kubectl apply -f server-deployment.yaml
+kubectl apply -f server-service.yaml
+kubectl apply -f server-hpa.yaml
+kubectl apply -f server-ingress.yaml
